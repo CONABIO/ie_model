274f07bf1cdc9b07e71d95c8f6abc0256c4bccd6
Karen Sanchez <karen.sanchez@conabio.gob.mx>
2024-11-27 16:07:06 -0600

    cambios menores

--- scripts/xgb_model/evaluate_accuracy.R
+++ scripts/xgb_model/evaluate_accuracy.R
@@ -2,11 +2,12 @@
 
 library('tidyverse')
 library('terra')
+library('caret')
 
-r_ei <- terra::rast('output/ie_xgb_slic/march/ie_xgb_slic_2017_march.tif')
-r_hemerobia <- terra::rast('output/models/xgb slic v7/hemerobia_slic.tif')
+r_ei <- terra::rast('output/ie_xgb_slic/march_sv_landcovercorrected/ie_xgb_slic_2017.tif')
+r_hemerobia <- terra::rast('data/model_input/slic/2017/hemerobia_slic.tif')
 
-df_is_train <- read_csv('output/models/xgb slic v7/is_train.csv') # Indicator of training
+df_is_train <- read_csv('output/models/xgb slic v10/is_train.csv') # Indicator of training
 slic_file <- 'data/model_input/slic/2017/slic.shp'
 is_slic <- TRUE
 
@@ -35,19 +36,22 @@ if(is_slic) {
 df <- terra::as.data.frame(c(r_hemerobia,
                              r_is_train,
                              r_ei),
-                     xy = TRUE, na.rm = TRUE) 
+                     xy = TRUE, na.rm = FALSE) 
+df <- df %>% 
+  filter(!is.na(hemerobia))
 
 # Separate in training and testing
 df_train <- df %>% 
   filter(is_train==1)
 df_test <- df %>% 
-  filter(is_train==0)
+  filter(is_train==0 
+         | is.na(is_train)
+         )
 
 # Estimate overall, training and testing accuracy
-mean(df$prediction==df$hemerobia)
-mean(df_train$prediction==df_train$hemerobia)
-mean(df_test$prediction==df_test$hemerobia)
-confusionMatrix(as.factor(df_test$hemerobia), as.factor(df_test$prediction))
+mean(df$prediction==df$hemerobia, na.rm=T)
+mean(df_train$prediction==df_train$hemerobia, na.rm=T)
+mean(df_test$prediction==df_test$hemerobia, na.rm=T)
 
 # Estimate accuracy and area covered stratified by holdridge
 df_accuracy_train <- df_train %>% 
@@ -60,3 +64,27 @@ df_accuracy_test <- df_test %>%
   group_by(holdridge) %>% 
   summarise(acc_pct = round(mean(prediction == hemerobia)*100,2),
             area_pct = round(n()/nrow(df_test)*100,2))
+
+# Confusion matrix
+cm <- confusionMatrix(factor(df_test$prediction), factor(df_test$hemerobia), 
+                      dnn = c("Prediction", "Reference"))
+
+plt <- as.data.frame(cm$table)
+plt <- plt %>% 
+  group_by(Reference) %>% 
+  mutate(n = sum(Freq)) %>% 
+  ungroup() %>% 
+  mutate(Freq = round((Freq/n)*100))
+
+ggplot(plt, aes(Prediction,Reference, fill= Freq)) +
+  geom_tile() + geom_text(aes(label=Freq)) +
+  scale_fill_gradient(low="white", high="blue") +
+  labs(x = "Prediction",y = "Reference") 
+
+
+# df_aux <- df_test %>% 
+#   mutate(mistake = ifelse((prediction == 1 & hemerobia == 4), 1, 0))
+# 
+# writeRaster(rast(df_aux %>%
+#                    select(c(x,y,mistake))), 
+#             "output/models/xgb slic v11/r_mistake_4.tif")
